4b8f2a36b5b3751cd82bc579ef99fe1441417294
batu <ba921@nyu.edu>
2015-10-08 16:56:32 -0400

    Wow using the commit tool! Sweet... Useless but sweet.

--- Assets/InsignaPlayer/Scripts/callStarter.cs
+++ Assets/InsignaPlayer/Scripts/callStarter.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class callStarter : MonoBehaviour {
 
+    public AudioClip phoneSound;
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,9 @@ public class callStarter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if ( !GetComponent<AudioSource>().isPlaying && Input.GetMouseButtonDown(0)) {
+            GetComponent<AudioSource>().PlayOneShot(phoneSound);
+        }
+        
 	}
 }
