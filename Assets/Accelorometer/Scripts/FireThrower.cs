ebb92ef9da7a9b2e433b3f976b376ff87201d80a
batu <ba921@nyu.edu>
2015-10-13 00:13:45 -0400

    costumes

--- Assets/Accelorometer/Scripts/FireThrower.cs
+++ Assets/Accelorometer/Scripts/FireThrower.cs
@@ -6,15 +6,25 @@ public class FireThrower : MonoBehaviour {
 
     public GameObject fireball;
     public float speed = 5f;
+    public float moveAmount = 5;
+    public float fireballFrequency = 3;
     Vector3 ObjectSpawnPosition;
     GameObject fireballInstance;
     
 
     void fireThrowerStart() {
 
-        fireballInstance = (GameObject)Instantiate(fireball);
-        fireballInstance.GetComponent<Rigidbody>().AddForce(new Vector3(0,0,1000f));
+        int random = Random.Range(0, 3);
+        if (random == 0) {
+            fireballInstance = (GameObject)Instantiate(fireball,ObjectSpawnPosition,Quaternion.identity);
+        } else if( random == 1) {
+            fireballInstance = (GameObject)Instantiate(fireball, ObjectSpawnPosition + new Vector3(moveAmount, 0,0), Quaternion.identity);
+        }else if(random == 2) { 
+            fireballInstance = (GameObject)Instantiate(fireball, ObjectSpawnPosition + new Vector3(-moveAmount, 0, 0), Quaternion.identity);
+        }
 
+
+        Debug.Log(fireballInstance.name);
         return;
     }
 
@@ -24,12 +34,13 @@ public class FireThrower : MonoBehaviour {
         ObjectSpawnPosition.y = transform.position.y;
         ObjectSpawnPosition.z = transform.position.z;
 
+        InvokeRepeating("fireThrowerStart", 2, fireballFrequency);
 
-        fireThrowerStart();
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        
+    }
 }
---
+++ Assets/Accelorometer/Scripts/playerMovement.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+    public int playerPosition = 0;
+    public int leftTreshold = -7000;
+    public int rightTreshold = 7000;
+    bool atRight = false;
+    bool atLeft = false;
+    bool positionChanged = false;
+    int oldPos = 0;
+    float accInput;
+
+    public float turnRotationAngle = 30f;
+    private float rotationVelocity;
+    public float turnRotationSeekSpeed = .45f;
+
+
+
+    void turnRight() {
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z = Mathf.SmoothDampAngle(newRotation.z, 1f * -turnRotationAngle, ref rotationVelocity, turnRotationSeekSpeed);
+        transform.eulerAngles = newRotation;
+    }
+
+    void turnLeft() {
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z = Mathf.SmoothDampAngle(newRotation.z, -1f * -turnRotationAngle, ref rotationVelocity, turnRotationSeekSpeed);
+        transform.eulerAngles = newRotation;
+    }
+
+    void backToMiddle() {
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z = Mathf.SmoothDampAngle(newRotation.z, 0f * -turnRotationAngle, ref rotationVelocity, turnRotationSeekSpeed);
+        transform.eulerAngles = newRotation;
+    }
+
+  
+    void Update () {
+
+        //"Fake" rotate the car
+
+
+
+
+        string comingInput = Input.inputString;
+        if (Input.inputString != "") {
+
+            if (comingInput[0] == '-') {
+                accInput = float.Parse(comingInput.TrimStart('-'));
+                accInput = -accInput;
+            } else {
+                accInput = float.Parse(comingInput);
+            }
+     
+            if (accInput < leftTreshold) {
+                Debug.Log("RIGHT");
+                atRight = true;
+                atLeft = false;
+                oldPos = playerPosition;
+                playerPosition = 1;
+            } else if (accInput > rightTreshold) {
+                Debug.Log("LEFT");
+                atLeft = true;
+                atRight = false;
+                oldPos = playerPosition;
+                playerPosition = -1;
+            } else if (accInput < rightTreshold && accInput > leftTreshold) {
+                Debug.Log("MIDDLE");
+                atRight = false;
+                atLeft = false;
+                playerPosition = 0;
+            }
+
+        }
+
+        if (atRight) {
+            turnRight();
+        } else if (atLeft) {
+            turnLeft();
+        } else {
+            backToMiddle();
+        }
+    }
+}
